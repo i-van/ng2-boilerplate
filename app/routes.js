6dab52f535046d767165f49347bfc6d19cbb738a
Ivan Nosov <i.k.nosov@gmail.com>
2016-07-13 17:57:07 +0300

    redirect to home route by default

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ import UsersComponent from './components/user/user.component';
 import UsersIndexComponent from './components/user/index/user-index.component';
 
 export let routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'users', component: UsersComponent, children: [
     { path: '', component: UsersIndexComponent }

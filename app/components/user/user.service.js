73bb00552e9882389f78bd275764f58b46cd1b57
Ivan Nosov <i.k.nosov@gmail.com>
2016-07-14 17:12:17 +0300

    rename login to email, log request data

--- app/components/user/user.service.js
+++ app/components/user/user.service.js
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 let data = [
-  { id: 1, login: 'user',      firstName: 'John', lastName: 'Daw',   created: new Date(1437142998061) },
-  { id: 2, login: 'admin',     firstName: 'Jane', lastName: 'Daw',   created: new Date(1460152988061) },
-  { id: 3, login: 'moderator', firstName: 'John', lastName: 'Smith', created: new Date(1463182978061) }
+  { id: 1, email: 'john.dow@example.com',   firstName: 'John', lastName: 'Daw',   created: new Date(1437142998061) },
+  { id: 2, email: 'jane.dow@example.com',   firstName: 'Jane', lastName: 'Daw',   created: new Date(1460152988061) },
+  { id: 3, email: 'john.smith@example.com', firstName: 'John', lastName: 'Smith', created: new Date(1463182978061) }
 ];
 
-function wrapper(data) {
+function wrapper(url, data) {
+  console.log('request', url);
+  console.log('response', data);
   return new Observable(observer => {
     setTimeout(() => {
       observer.next(data);
@@ -19,10 +21,10 @@ function wrapper(data) {
 @Injectable()
 export default class UserService {
   fetch() {
-    return wrapper(data);
+    return wrapper('/users', data);
   }
 
   fetchOne(id: Number) {
-    return wrapper(data.find(user => user.id === id));
+    return wrapper('/users/' + id, data.find(user => user.id === id));
   }
 }
